051b0b8886bfa1f82691d8539b87fd9bf940cac4
mohamed-taher7 <64607801+mohamed-taher7@users.noreply.github.com>
2020-08-22 12:04:15 +0200

    var and globals

--- variables.py
+++ variables.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python
-
 # Python Variables
 
 # Creating Variables
@@ -36,3 +34,14 @@ print(x + y)
 x = 5
 y = "John"
 print(x + y)
+#var names:A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ ) (case sensitive and don't start with num)
+#If you use the global keyword, the variable belongs to the global scope:
+def myfunc():
+  global x
+  x = "fantastic"
+
+myfunc()
+
+print("Python is " + x) 
+#----------------------------------------------#
+#Also, use the global keyword if you want to change a global variable inside a function.
