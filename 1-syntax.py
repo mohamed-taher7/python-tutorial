7a843c0858726184f3f1af948cde13c60d21204d
Mohamed Taher <64607801+mohamed-taher7@users.noreply.github.com>
2020-08-24 15:09:43 +0200

    Syntax, comments, variables, Import library.

---
+++ 1-syntax.py
@@ -0,0 +1,13 @@
+# 1. Comments
+# This is a comment.
+print("Hello, World!")
+
+# 2. Docstrings
+""" This is a
+multiline docstring."""
+
+# 3. Indentations
+if 5 > 2:
+    print("Five is greater than two!")
+
+# 4. variables
