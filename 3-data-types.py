e8ba1ad1c91b3fa9301876f4dfe613194de209fa
Mohamed Taher <64607801+mohamed-taher7@users.noreply.github.com>
2020-08-24 15:38:35 +0200

    Data types, Numbers

--- 3-data-types.py
+++ 3-data-types.py
@@ -1,13 +1,15 @@
 print("data types by examples")
-"""
-Text Type: 	str
+#__________________________________________________#
+# Text Type: 	str
 m = "Hello World"
+#__________________________________________________#
+# Numeric Types: 	int, float, complex
+x, y, z = 1, 35656222554887711, -3255522      # int:  whole number, positive or negative, without decimals, of unlimited length.
+a, b, c = 2.8, -35.59, -87.7e100 # float: positive or negative, containing one or more decimals.
+z = 3-51j      # complex
 
-Numeric Types: 	int, float, complex
-x = 1       # int
-y = 2.8     # float
-z = 1j      # complex
----------
+#Type Conversion
+
 #convert from int to float:
 a = float(x)
 
@@ -16,29 +18,31 @@ b = int(y)
 
 #convert from int to complex:
 c = complex(x)
-Note: You cannot convert complex numbers into another number type.
-to make random num
+# Note: You cannot convert complex numbers into another number type.
+
+# to make random num
 import random
 
 print(random.randrange(1, 10)) 
------------------
-Sequence Types: 	list, tuple, range
+#__________________________________________________#
+# Sequence Types: 	list, tuple, range
 x = ["apple", "banana", "cherry"]   #list: ordered and changeable. Allows duplicate members
 y = ("apple", "banana", "cherry")   #tuple:  ordered and unchangeable. Allows duplicate members.
 x = range(6)                        #range(start, stop, step) 
-Mapping Type: 	dict
+
+# Mapping Type: 	dict
 a = {"name" : "John", "age" : 36}    #dict: unordered, changeable and indexed. No duplicate members.
 
-Set Types: 	set, frozenset
+# Set Types: 	set, frozenset
 z = {"apple", "banana", "cherry"}   #set: unordered and unindexed. No duplicate members.
 b = frozenset({"apple", "banana", "cherry"}) #immutable version of a Python set
 
-Boolean Type: 	bool
+# Boolean Type: 	bool
 m = True
 l = False
 
-Binary Types: 	bytes, bytearray, memoryview
+# Binary Types: 	bytes, bytearray, memoryview
 x = b"Hello"
 x = bytearray(5)
 x = memoryview(bytes(5))
-"""
+
