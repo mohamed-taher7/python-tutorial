e1918f77cf267ccb173320f62f1d97874bbcbb86
Mohamed Taher <64607801+mohamed-taher7@users.noreply.github.com>
2020-08-25 09:03:26 +0200

    Update and rename 1-syntax.py to 1-syntax-'n-var.py

---
+++ 1-syntax-'n-var.py
@@ -0,0 +1,36 @@
+# 1. Comments
+# This is a comment.
+print("Hello, World!")
+
+# 2. Docstrings
+""" This is a
+multiline docstring."""
+
+# 3. Indentations is a must in python
+if 5 > 2:
+    print("Five is greater than two!") 
+if 5 > 2:
+print("Five is greater than two!") #doesn't work
+if 5 > 2:
+        print("Five is greater than two!") 
+    print("six is greater than two!")#doesn't work
+
+# 4. Variables
+x = 5
+
+    """
+Operator 	Example 	Same As
+= 	    x = 5 	        x = 5 	
++=  	x += 3      	x = x + 3 	
+-= 	    x -= 3      	x = x - 3 	
+*=  	x *= 3      	x = x * 3 	
+/=  	x /= 3      	x = x / 3 	
+%= 	    x %= 3      	x = x % 3 	
+//= 	x //= 3 	    x = x // 3 	
+**= 	x **= 3 	    x = x ** 3 	
+&=  	x &= 3      	x = x & 3 	
+|= 	    x |= 3      	x = x | 3 	
+^= 	    x ^= 3 	        x = x ^ 3 	
+>>= 	x >>= 3 	    x = x >> 3 	
+<<= 	x <<= 3     	x = x << 3
+"""
