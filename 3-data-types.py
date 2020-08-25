abebdf9e7b1c5b9552505db1b40381000a3a6300
Mohamed Taher <64607801+mohamed-taher7@users.noreply.github.com>
2020-08-25 08:54:31 +0200

    Update 3-data-types.py

--- 3-data-types.py
+++ 3-data-types.py
@@ -1,5 +1,7 @@
-print("data types by examples")
-#__________________________________________________#
+"""
+Data-types with some details
+"""
+#______________________Strings___________________________#
 # Text Type: 	str
 m = "Hello World"
 
@@ -98,8 +100,10 @@ print("We are the so-called \"Vikings\" from the north.") #output: We are the so
 \xhh 	Hex value
 """
 
+#convert from int/float/comlex to string:
+a = str(x)
 
-#__________________________________________________#
+#______________________Numbers____________________________#
 # Numeric Types: 	int, float, complex
 x, y, z = 1, 35656222554887711, -3255522      # int:  whole number, positive or negative, without decimals, of unlimited length.
 a, b, c = 2.8, -35.59, -87.7e100 # float: positive or negative, containing one or more decimals.
@@ -111,17 +115,25 @@ z = 3-51j      # complex
 a = float(x)
 
 #convert from float to int:
-b = int(y)
+b = int(y) #1.0 will be 1
 
 #convert from int to complex:
-c = complex(x)
+c = complex(x, l) #l is img part if exist
 # Note: You cannot convert complex numbers into another number type.
+"""
+Note:When converting from a string, the string must not contain whitespace around the central + or - operator.
+For example, complex('1+2j') is fine, 
+but complex('1 + 2j') raises ValueError.
+"""
 
 # to make random num
 import random
 
 print(random.randrange(1, 10)) 
-#__________________________________________________#
+
+
+
+#______________________Sequences____________________________#
 # Sequence Types: 	list, tuple, range
 x = ["apple", "banana", "cherry"]   #list: ordered and changeable. Allows duplicate members
 y = ("apple", "banana", "cherry")   #tuple:  ordered and unchangeable. Allows duplicate members.
@@ -134,9 +146,47 @@ a = {"name" : "John", "age" : 36}    #dict: unordered, changeable and indexed. N
 z = {"apple", "banana", "cherry"}   #set: unordered and unindexed. No duplicate members.
 b = frozenset({"apple", "banana", "cherry"}) #immutable version of a Python set
 
+#______________________Booleans____________________________#
 # Boolean Type: 	bool
 m = True
 l = False
+"""
+Any string is True, except empty strings.
+Any number is True, except 0.
+Any list, tuple, set, and dictionary are True, except empty ones.
+"""
+bool("mediate") #output: True
+#note: Functions can Return a Boolean
+"""
+Operators for booleans
+
+== 	Equal 	x == y 	
+!= 	Not equal 	x != y 	
+> 	Greater than 	x > y 	
+< 	Less than 	x < y 	
+>= 	Greater than or equal to 	x >= y 	
+<= 	Less than or equal to 	x <= y
+
+and  	  Returns True if both statements are true 	x < 5 and  x < 10 	
+or 	    Returns True if one of the statements is true 	x < 5 or x < 4 	
+not 	  Reverse the result, returns False if the result is true 	not(x < 5 and x < 10) 	
+is  	  Returns True if both variables are the same object 	x is y 	
+is not  Returns True if both variables are not the same object
+in  	  Returns True if a sequence with the specified value is present in the object 	x in y 	
+not in 	Returns True if a sequence with the specified value is not present in the object 	x not in y 	
+"""
+
+#EX
+print(6>=9) #False
+
+x = ["apple", "banana"]
+
+print("banana" in x)
+
+# returns True because a sequence with the value "banana" is in the list
+
+
+#______________________Binary Types____________________________#
 
 # Binary Types: 	bytes, bytearray, memoryview
 x = b"Hello"
