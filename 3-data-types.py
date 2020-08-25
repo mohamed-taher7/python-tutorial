db2077337a96e9194df2b3af65f62acbb6bb4c04
Mohamed Taher <64607801+mohamed-taher7@users.noreply.github.com>
2020-08-25 10:11:52 +0200

    Update 3-data-types.py

--- 3-data-types.py
+++ 3-data-types.py
@@ -131,6 +131,17 @@ import random
 
 print(random.randrange(1, 10)) 
 
+"""
+ Operator              Name                Example
+   +                 Addition               x + y
+   -                 Subtraction            x - y
+   *                 Multiplication         x * y
+   /                 Division               x / y
+   %                 Modulus                x % y
+  **                 Exponentiation         x ** y
+  //                 Floor division         x // y
+"""
+
 
 
 #______________________Sequences____________________________#
