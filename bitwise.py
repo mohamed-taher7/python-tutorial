727a1b707fd7516764cf35a8b5ee7642bc3da84b
Mohamed Taher <64607801+mohamed-taher7@users.noreply.github.com>
2020-08-26 14:52:18 +0200

    Create bitwise.py

---
+++ bitwise.py
@@ -0,0 +1,20 @@
+"""
+Bitwise is about binary operations.
+more: https://en.wikipedia.org/wiki/Bitwise_operation
+
+==================================
+variables assignment
+      example   same as
+>>= 	x >>= 3 	x = x >> 3 	
+<<= 	x <<= 3 	x = x << 3 	
+
+==================================
+operators   name  description
+&         	AND 	Sets each bit to 1 if both bits are 1
+|        	  OR 	Sets each bit to 1 if one of two bits is 1
+ ^ 	        XOR 	Sets each bit to 1 if only one of two bits is 1
+~       	  NOT 	Inverts all the bits
+<<       	  Zero fill left shift 	Shift left by pushing zeros in from the right and let the leftmost bits fall off
+>>      	  Signed right shift 	Shift right by pushing copies of the leftmost bit in from the left, and let the rightmost bits fall off
+
+"""
