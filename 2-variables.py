c2905f290b4c59ed579123e366227ac82577c4ac
Mohamed Taher <64607801+mohamed-taher7@users.noreply.github.com>
2020-08-26 14:25:48 +0200

    Update 2-variables.py

--- 2-variables.py
+++ 2-variables.py
@@ -34,23 +34,20 @@ print(x + y)
 # Assignment Operators
 
 """
-
-
 Operator 	Example 	Same As
-= 	    x = 5 	        x = 5 	
-+=  	x += 3      	x = x + 3 	
--= 	    x -= 3      	x = x - 3 	
-*=  	x *= 3      	x = x * 3 	
-/=  	x /= 3      	x = x / 3 	
-%= 	    x %= 3      	x = x % 3 	
-//= 	x //= 3 	    x = x // 3 	
-**= 	x **= 3 	    x = x ** 3 	
-&=  	x &= 3      	x = x & 3 	
-|= 	    x |= 3      	x = x | 3 	
-^= 	    x ^= 3 	        x = x ^ 3 	
->>= 	x >>= 3 	    x = x >> 3 	
-<<= 	x <<= 3     	x = x << 3
-
+= 	x = 5 	x = 5 	
++= 	x += 3 	x = x + 3 	
+-= 	x -= 3 	x = x - 3 	
+*= 	x *= 3 	x = x * 3 	
+/= 	x /= 3 	x = x / 3 	
+%= 	x %= 3 	x = x % 3 	
+//= 	x //= 3 	x = x // 3 	
+**= 	x **= 3 	x = x ** 3 	
+&= 	x &= 3 	x = x & 3 	
+|= 	x |= 3 	x = x | 3 	
+^= 	x ^= 3 	x = x ^ 3 	
+>>= 	x >>= 3 	x = x >> 3 	
+<<= 	x <<= 3 	x = x << 3 	
 
 """
 
