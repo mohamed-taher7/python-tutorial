35dc4af94ec416d5fef3daf99b390b315393e90d
Mohamed Taher <64607801+mohamed-taher7@users.noreply.github.com>
2020-08-28 19:48:17 +0200

    Update and rename loops.py to 5-1-for-loops.py

--- 5-1-for-loops.py
+++ 5-1-for-loops.py
@@ -1,13 +1,5 @@
-# Python has two primitive loop commands:
-#
-# while loops
-# for loops
-#---------------
-# Python For Loops
-#
-# A for loop is used for iterating over a sequence (that is either a list, a tuple, a dctionary, a set, or a string).
-# This is less like the for keyword in other programming language, and works more like an iterator method as found in other object-orientated programming languages.
-# With the for loop we can execute a set of statements, once for each item in a list, tuple, set etc.
+#_____________________________________for loop_____________________________________#
+# for loop: iterating(repeating) over a sequence (that is either a list, a tuple, a dctionary, a set, or a string).
 
 fruits = ["apple", "banana", "cherry"]
 for x in fruits:
@@ -16,17 +8,19 @@ for x in fruits:
 # The for loop does not require an indexing variable to set beforehand.
 
 #Loop Through a String
-#
-# Even strings are iterable objects, they contain a sequence of characters:
 for x in "banana":
     print(x)
-
+    
+    
+#Exit the loop when x is "banana":
 fruits = ["apple", "banana", "cherry"]
 for x in fruits:
     print(x)
     if x == "banana":
         break
-
+        
+        
+#Exit the loop when x is "banana", but this time the break comes before the print:(banana is not printed)
 fruits = ["apple", "banana", "cherry"]
 for x in fruits:
     if x == "banana":
@@ -38,24 +32,23 @@ fruits = ["apple", "banana", "cherry"]
 for x in fruits:
     if x == "banana":
         continue
-    print(x)
+    print(x) #doesn't print banana but continues loop
 
 # The range() Function
-#
-# To loop through a set of code a specified number o fimtes, we can use the range() function,
-# The range() function returns a sequence of numbers, starting from 0 by default, and increments by 1 (by default), and ends at a specified number.
+# To loop through a set of code a specified number of timtes, we can use the range() function,
+# The range() starts from 0 by default, and increments by 1 (by default), and ends at a specified number.
 
 for x in range(6):
     print(x)
 
 # Note that range(6) is not the values of 0 to 6, but the values 0 to 5.
 
-# The range() function defaults to 0 as a starting value, however it is possible to specify the starting value by addin a parameter:
+
 # range(2, 6), which means values from 2 to 6 (but no including 6):
 for x in range(2, 6):
     print(x)
 
-# The range() function defaults to increment the sequence by 1, however it is possible to specify the increment value by adding a third parameter: range(2, 30, 3):
+# increment value by 3
 for x in range(2, 30, 3):
     print(x)
 
