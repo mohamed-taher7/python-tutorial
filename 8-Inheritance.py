2e83929cd60a31dc45c89a00713efaa288a4454a
Mohamed Taher <64607801+mohamed-taher7@users.noreply.github.com>
2020-09-02 00:30:16 +0200

    Update 8-Inheritance.py

--- 8-Inheritance.py
+++ 8-Inheritance.py
@@ -1,51 +1,59 @@
-#__________________________________Inheritance________________________________#
-
-#Inheritance allows us to define a class that inherits all the methods and properties from another class.
+#____________________________________inheritance_______________________________#
 
 """
-Parent class is the class being inherited from, also called base class.
-
-Child class is the class that inherits from another class, also called derived class.
+Inheritance allows us to define a class that inherits all the methods and properties from another class.
+Parent class(Any class can be a parent class): class being inherited from, also called base class.
+Child class: class inherits from another class, also called derived class.
 """
-
-#Parent Class(Any class can be a parent):
-
 class Person:
-  def __init__(self, fname, lname):
-    self.firstname = fname
-    self.lastname = lname
+  def __init__(lol, fname, lname):
+    lol.firstname = fname #it's name is lol to diffrentiate between it and the next "self" only
+    lol.lastname = lname
+    print ("the __init__ func. is run immeadiately after class object is created(whether it ran or not)")
 
   def printname(self):
-    print(self.firstname, self.lastname)
+    print(self.firstname +" "+ self.lastname)
 
 #Use the Person class to create an object, and then execute the printname method:
 
-x = Person("John", "Doe")
-x.printname() 
+x = Person("John", "Doe") #class object is created
+#try above with this command
+#x.printname()
 
-#Create a class named Student, which will inherit the properties and methods from the Person class:
-#send the parent class as a parameter to inherit
-
- class Student(Person):
-  pass 
-
+#child class(by passing class as variable):
+class Student(Person): #Now the Student class has the same properties and methods as the Person class
+  pass
 x = Student("Mike", "Olsen")
-x.printname() 
-"""
-Note: The __init__() function is called automatically every time the class is being used to create a new object.
+x.printname()
 
-When you add the __init__() function, the child class will no longer inherit the parent's __init__() function.
+#When you add the __init__() function, the child class will no longer inherit the parent's __init__() function.
 
-To keep the inheritance of the parent's __init__() function, add a call to the parent's __init__() function:
+class Student(Person):
+  def __init__(self, fname, lname): 
+    #add properties etc. 
 
-"""
+
+#To keep the inheritance of the parent's __init__() function, add a call to the parent's __init__() function:
 class Student(Person):
   def __init__(self, fname, lname):
     Person.__init__(self, fname, lname) 
- 
- #or with the super() Function
- 
- class Student(Person):
+#OR
+class Student(Person):
   def __init__(self, fname, lname):
     super().__init__(fname, lname) 
- 
+
+#Add a property called graduationyear to the Student class:
+class Student(Person):
+  def __init__(self, fname, lname):
+    super().__init__(fname, lname)
+    self.graduationyear = 2019
+x = Student("Mike", "Olsen", 2019) 
+
+#Add a method called welcome to the Student class:
+class Student(Person):
+  def __init__(self, fname, lname, year):
+    super().__init__(fname, lname)
+    self.graduationyear = year
+
+  def welcome(self):
+    print("Welcome", self.firstname, self.lastname, "to the class of", self.graduationyear) 
