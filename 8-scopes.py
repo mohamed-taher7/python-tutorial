301ae884efd45695762b133a83533dea1ea5dc91
Mohamed Taher <64607801+mohamed-taher7@users.noreply.github.com>
2020-09-02 18:08:19 +0200

    Update 8-scopes.py

--- 8-scopes.py
+++ 8-scopes.py
@@ -45,4 +45,31 @@ myfunc()
 
 print(x) 
 
-#To change the value of a global variable inside a function, refer to the variable by using the global keyword:
+#To change the value of a global variable inside a function, refer to the variable by using the global keyword.
+
+
+#classes:
+
+def scope_test():
+    def do_local():
+        spam = "local spam"
+
+    def do_nonlocal():
+        nonlocal spam
+        spam = "nonlocal spam"
+
+    def do_global():
+        global spam
+        spam = "global spam"
+
+    spam = "test spam"
+    do_local()
+    print("After local assignment:", spam)
+    do_nonlocal()
+    print("After nonlocal assignment:", spam)
+    do_global()
+    print("After global assignment:", spam)
+
+scope_test()
+print("In global scope:", spam)
+
