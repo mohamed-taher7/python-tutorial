6dcda34f16aac3b9b438feaa53a8539c4d29faea
Mohamed Taher <64607801+mohamed-taher7@users.noreply.github.com>
2020-09-02 17:30:41 +0200

    Create 10-scopes.py

---
+++ 10-scopes.py
@@ -0,0 +1,48 @@
+#______________________________scopes__________________________________________#
+
+#A variable is only available from inside the region it is created. This is called scope.
+
+#The local variable can be accessed from a function within the function:
+def myfunc():
+  x = 300
+  def myinnerfunc():
+    print(x)
+  myinnerfunc()
+
+myfunc()
+
+#A variable created outside of a function is global and can be used by anyone:
+
+x = 300
+
+def myfunc():
+  print(x)
+
+myfunc()
+
+print(x) 
+
+#The function will print the local x, and then the code will print the global x:
+
+The function will print the local x, and then the code will print the global x:
+x = 300
+
+def myfunc():
+  x = 200
+  print(x)
+
+myfunc()
+
+print(x) 
+
+#If you use the global keyword, the variable belongs to the global scope:
+
+def myfunc():
+  global x
+  x = 300
+
+myfunc()
+
+print(x) 
+
+#To change the value of a global variable inside a function, refer to the variable by using the global keyword:
