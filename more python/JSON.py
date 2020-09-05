7243951f6d302afa18b7d63838102d2fc1624347
Mohamed Taher <64607801+mohamed-taher7@users.noreply.github.com>
2020-09-05 21:29:49 +0200

    Update JSON.py

--- more python/JSON.py
+++ more python/JSON.py
@@ -1,21 +1,25 @@
 """
-JSON stands for JavaScript Object Notation
-JSON is a lightweight format for storing and transporting data
-JSON is often used when data is sent from a server to a web page
-JSON is "self-describing" and easy to understand
+JSON:
+    stands for JavaScript Object Notation.
+    is a lightweight format for storing and transporting data.
+    is often used when data is sent from a server to a web page.
+    is "self-describing" and easy to understand
 """
 
-#Convert from JSON to Python:
+#Import the json module:
 import json
 
-# some JSON:
+#Parse JSON (i.e.: Convert from JSON to Python) with json.loads()
 x =  '{ "name":"John", "age":30, "city":"New York"}'
-
-# parse x:
-y = json.loads(x)
-
-# the result is a Python dictionary:
+y = json.loads(x)# the result is a Python dictionary:
 print(y["age"]) 
+
+#from Python to JSON with json.dumps()
+x =  { "name":"John", "age":30, "city":"New York"}
+# convert into JSON:
+y = json.dumps(x)
+print(y) #output is sring: {"name": "John", "age": 30, "city": "New York"}
+
 """
 You can convert Python objects of the following types, into JSON strings:
 
@@ -28,6 +32,118 @@ You can convert Python objects of the following types, into JSON strings:
     True
     False
     None
+    
+When you convert from Python to JSON, Python objects are converted into the JSON (JavaScript) equivalent:
+Python 	JSON
+
+dict 	Object
+list 	Array
+tuple 	Array
+str 	String
+int 	Number
+float 	Number
+True 	true
+False 	false
+None 	null
+
+
+
+"""
+#Convert a Python object containing all the legal data types:
+import json
+
+x = {
+  "name": "John",
+  "age": 30,
+  "married": True,
+  "divorced": False,
+  "children": ("Ann","Billy"),
+  "pets": None,
+  "cars": [
+    {"model": "BMW 230", "mpg": 27.5},
+    {"model": "Ford Edge", "mpg": 24.1}
+  ]
+}
+
+print(json.dumps(x)) #tryit to understand the next
+
+#Use the indent parameter to define the numbers of indents:
+json.dumps(x, indent=4)
+
+"""
+output:
+{
+    "name": "John",
+    "age": 30,
+    "married": true,
+    "divorced": false,
+    "children": [
+        "Ann",
+        "Billy"
+    ],
+    "pets": null,
+    "cars": [
+        {
+            "model": "BMW 230",
+            "mpg": 27.5
+        },
+        {
+            "model": "Ford Edge",
+            "mpg": 24.1
+        }
+    ]
+} 
+"""
+# use . and a space to separate objects, and a space, a = and a space to separate keys from their values:
+print(json.dumps(x, indent=4, separators=(". ", " = ")))
+"""
+output:
+{
+    "name" = "John".
+    "age" = 30.
+    "married" = true.
+    "divorced" = false.
+    "children" = [
+        "Ann".
+        "Billy"
+    ],
+    "pets" = null.
+    "cars" = [
+        {
+            "model" = "BMW 230".
+            "mpg" = 27.5
+        }.
+        {
+            "model" = "Ford Edge".
+            "mpg" = 24.1
+        }
+    ]
+} 
 """
 
->>>>>>>>>>>>>>>CONTINUE
+#Use the sort_keys parameter to specify if the result should be sorted or not:
+json.dumps(x, indent=4, sort_keys=True)
+
+"""
+{
+    "age": 30,
+    "cars": [
+        {
+            "model": "BMW 230",
+            "mpg": 27.5
+        },
+        {
+            "model": "Ford Edge",
+            "mpg": 24.1
+        }
+    ],
+    "children": [
+        "Ann",
+        "Billy"
+    ],
+    "divorced": false,
+    "married": true,
+    "name": "John",
+    "pets": null
+}
+"""
