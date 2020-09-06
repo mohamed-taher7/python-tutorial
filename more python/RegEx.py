23d9667f823b4438624d3218d6f31ccd89b6fc5e
Mohamed Taher <64607801+mohamed-taher7@users.noreply.github.com>
2020-09-06 18:04:16 +0200

    Update RegEx.py

--- more python/RegEx.py
+++ more python/RegEx.py
@@ -6,11 +6,16 @@ https://regexone.com/
 
 import re #regex module
 
-Search the string to see if it starts with "The" and ends with "Spain":
-import re
+#Search the string to see if it starts with "The" and ends with "Spain":
 
 txt = "The rain in Spain"
 x = re.search("^The.*Spain$", txt) 
+"""
+Function 	Description
 
-
->>>>>>>>>>>>>>>>>>>>>>here
+findall 	Returns a list containing all matches
+search 	  Returns a Match object if there is a match anywhere in the string
+split 	  Returns a list where the string has been split at each match
+sub 	    Replaces one or many matches with a string
+"""
+>>>>>>>learn regex
