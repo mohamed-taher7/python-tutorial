218dc65461d3f9494bc8f3883fdfc201fce8b852
Mohamed Taher <64607801+mohamed-taher7@users.noreply.github.com>
2020-09-08 23:36:26 +0200

    Create 04-shape.py

---
+++ numpy/04-shape.py
@@ -0,0 +1,7 @@
+#Print the shape of a 2-D array:
+import numpy as np
+
+arr = np.array([[1, 2, 3, 4], [5, 6, 7, 8]])
+
+print(arr.shape) 
+#The example above returns (2, 4), which means that the array has 2 dimensions, and each dimension has 4 elements.
