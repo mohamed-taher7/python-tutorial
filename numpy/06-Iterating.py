7d2ea3a85619d2d1caf6a7f85bb28ea8d2129871
Mohamed Taher <64607801+mohamed-taher7@users.noreply.github.com>
2020-09-11 20:38:28 +0200

    Update 06-Iterating.py

--- numpy/06-Iterating.py
+++ numpy/06-Iterating.py
@@ -47,4 +47,32 @@ for x in np.nditer(arr[:, ::2]):
 5
 7
 '''
->>>>>>>>>>>>>>>>>>>>>>>continue
+
+#Enumeration: sequence number of somethings one by one.
+#1D
+
+arr = np.array([1, 2, 3])
+
+for idx, x in np.ndenumerate(arr):
+  print(idx, x)
+'''
+(0,) 1
+(1,) 2
+(2,) 3
+'''
+#2D
+
+arr = np.array([[1, 2, 3, 4], [5, 6, 7, 8]])
+
+for idx, x in np.ndenumerate(arr):
+  print(idx, x)
+'''
+(0, 0) 1
+(0, 1) 2
+(0, 2) 3
+(0, 3) 4
+(1, 0) 5
+(1, 1) 6
+(1, 2) 7
+(1, 3) 8
+'''
