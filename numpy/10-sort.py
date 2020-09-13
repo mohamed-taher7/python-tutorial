4b39d61db41816f5cbcd7aa314f8340409baf399
Mohamed Taher <64607801+mohamed-taher7@users.noreply.github.com>
2020-09-13 21:44:43 +0200

    Create 10-sort.py

---
+++ numpy/10-sort.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+arr = np.array([3, 2, 0, 1])
+
+print(np.sort(arr)) 
+'''
+bool: False first
+'''
+#Sort a 2-D array:
+arr = np.array([[3, 2, 4], [5, 0, 1]])
+
+print(np.sort(arr)) 
