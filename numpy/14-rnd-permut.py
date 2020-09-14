4d8596924520d1498e09bf369ec38bbc28d1d9be
Mohamed Taher <64607801+mohamed-taher7@users.noreply.github.com>
2020-09-14 11:47:54 +0200

    Create 14-rnd-permut.py

---
+++ numpy/14-rnd-permut.py
@@ -0,0 +1,14 @@
+'''
+shuffle() changes original array.
+'''
+from numpy import random
+import numpy as np
+
+arr = np.array([1, 2, 3, 4, 5])
+
+random.shuffle(arr)
+
+'''
+permutation() method returns a new re-arranged array
+'''
+arr = np.array([1, 2, 3, 4, 5])
